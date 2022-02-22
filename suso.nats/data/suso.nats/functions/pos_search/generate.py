292a0fb93822675e116a11e721b50702486f5bc0
Suso <41950530+Suso1@users.noreply.github.com>
2022-02-22 16:22:03 +0100

    Improve binary search generation

--- suso.nats/data/suso.nats/functions/pos_search/generate.py
+++ suso.nats/data/suso.nats/functions/pos_search/generate.py
@@ -1,8 +1,17 @@
-for i in range(32):
+# Configure up here
+BITS     = 5
+MIN_DIST = 27
+MAX_DIST = 120
+#------------------
+
+_AMOUNT = pow(2, BITS)
+_STEP = (MAX_DIST - MIN_DIST) / (_AMOUNT - 1)
+
+for i in range(_AMOUNT):
     start = 0
-    end = 32
+    end = _AMOUNT
     path = "s"
-    for s in range(5):
+    for s in range(BITS):
         mid = (start + end) / 2
         with open('./{}.mcfunction'.format(path), "w") as f:
            f.write('execute if score $s suso.nats matches ..{} run function suso.nats:pos_search/{}0\n'.format(int(mid - 1), path))
@@ -15,4 +24,4 @@ for i in range(32):
             path += "1"
     print(path)
     with open('./{}.mcfunction'.format(path), "w") as f:
-           f.write('execute positioned ^ ^ ^{} align xyz positioned ~.5 ~ ~.5 run function suso.nats:try'.format(24.0 + 3 * (i + 1)))
+           f.write('execute positioned ^ ^ ^{} align xyz positioned ~.5 ~ ~.5 run function suso.nats:try'.format(MIN_DIST + _STEP * i))
